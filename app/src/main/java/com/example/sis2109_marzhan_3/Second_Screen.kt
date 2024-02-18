47c575b0c94f4840f00932400e0eac56837a9284
Makonya2003 <makosia200323@gmail.com>
2024-02-19 00:43:58 +0600

    first change

---
+++ app/src/main/java/com/example/sis2109_marzhan_3/Second_Screen.kt
@@ -0,0 +1,12 @@
+package com.example.sis2109_marzhan_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Second_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_screen)
+
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "sis2109_Marzhan_3"
+include(":app")
+ 
